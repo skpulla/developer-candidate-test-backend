cd14d19284e83854f868b128ced83ba2767d02ca
Sudhi Pulla <sudheendrakumar@gmail.com>
2019-04-28 21:36:14 +1200

    Added support for filtering people

--- src/controllers/peopleController.js
+++ src/controllers/peopleController.js
@@ -7,11 +7,16 @@ const People = new Datastore({
   autoload: true,
 });
 
-function getPeople(req, res) {
-  People.find({}).skip(req.query.skip).limit(req.query.limit).exec((err, docs) => {
-    res.contentType('application/json');
-    res.status(200).send(docs);
-  });
+function getPeople(req, res, filters) {
+  People.find(
+    filters,
+  ).sort()
+    .skip(req.query.skip ? req.query.skip : req.query.offset)
+    .limit(req.query.limit)
+    .exec((err, docs) => {
+      res.contentType('application/json');
+      res.status(200).send(docs);
+    });
 }
 
 export default {
