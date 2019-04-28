2b6b3803e28d804c9f822d13b28892f86c8a954c
Sudhi Pulla <sudheendrakumar@gmail.com>
2019-04-29 03:59:35 +1200

    Enable All CORS requests

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import swaggerUi from 'swagger-ui-express';
@@ -8,6 +9,8 @@ import winston from './config/winston';
 
 const app = express();
 
+// TODO: Restrict CORS for better security
+app.use(cors());
 app.use(morgan('combined', { stream: winston.stream }));
 
 app.get('/', (req, res) => res.send('Welcome to Ambit API'));
