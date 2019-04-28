7d5b5958a3de0280746d17b4bdde4708d107424c
Sudhi Pulla <sudheendrakumar@gmail.com>
2019-04-28 14:47:23 +1200

    Added Winston and Morgan loggers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import morgan from 'morgan';
+import winston from './config/winston';
 
 const app = express();
+
+app.use(morgan('combined', { stream: winston.stream }));
 
 app.get('/', (req, res) => res.send('Received a GET HTTP method'));
 
@@ -13,5 +17,5 @@ app.delete('/', (req, res) => res.send('Received a DELETE HTTP method'));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+  winston.info(`Example app listening on port ${port}!`);
 });
